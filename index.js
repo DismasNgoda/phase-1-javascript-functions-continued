344e72dd43f0dd8577324e1503e0ee8f357127a1
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-08-21 22:15:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(a = "roller-skate"){
+    return `This Saturday, I want to ${a}!`;
+};
+
+
+function mondayWork(b = "go to the office"){
+    return `This Monday, I will ${b}.`    
+};
+
+function wrapAdjective(d = "*"){
+
+    return function (c = "a hard worker") {
+ 
+        return `You are ${d}${c}${d}!`;
+     }
+    
+   }
